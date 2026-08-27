de5ae85ede52ec7a19d5bb9c9e4066c793737b84
simoscat <simone.scattoni.ss@gmail.com>
2026-08-27 13:25:58 +0200

    modificato un test

--- src/test/java/engineering/TestEmailChecker.java
+++ src/test/java/engineering/TestEmailChecker.java
@@ -1,24 +1,30 @@
 package engineering;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestEmailChecker {
 
-    @ParameterizedTest
-    @CsvSource({
-            "'jane  doe@f.com', false", // spaces shouldn't be allowed
-            "'jane@@doe.com', false",
-            "'janedoe.com', false",
-            "'jane@doe.com', true"
-    })
-    void testEmailValidity(String email, boolean expected){
+    @Test
+    void testInvalidEmail(){
+
+        String email = "jane@@doe.com";
 
         boolean output = EmailChecker.isValidEmail(email);
 
-        assertEquals(expected, output);
+        assertEquals(false, output);
+
+    }
+
+    @Test
+    void testValidEmail(){
+
+        String email = "jane@doe.com";
+
+        boolean output = EmailChecker.isValidEmail(email);
+
+        assertEquals(true, output);
 
     }
 
